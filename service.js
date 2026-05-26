683fd9d84175470115eddb8ff9ff8fea1157e8c7
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-05-26 17:06:31 +0300

    defined the basic routes and port number

---
+++ service.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const port = 8000;
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send('Hello world');
+});
+
+
+app.listen(port,()=>{
+   console.log( `app runs on http://localhost:${port} `);
+});
