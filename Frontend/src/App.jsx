a6dbe94b1238cbc226354cdf842ee15a949b96d4
Wainaina Erastus <wainainaerastus2@gmail.com>
2026-09-03 17:51:08 +0300

    added the react-router-dom to the app for easy navigation to different page sections

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes,Route } from "react-router-dom";
+import ResetPassword from './pages/ResetPassword';
+import EmailVerify from './pages/EmailVerify';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div >App</div>
+    <div >
+    <Routes>
+      <Route  path='/'   element={<Home/>}/>
+      <Route  path='/login'   element={<Login/>}/>
+      <Route  path='/emailverify'   element={<EmailVerify/>}/>
+      <Route  path='/resetpassword'   element={<ResetPassword/>}/>
+    </Routes>
+    </div>
   )
 }
 
